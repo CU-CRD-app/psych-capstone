f6dab67da23a1e7eda9463ed21c3bb79754c8ee5
Alex Sandridge <asandridge@spotx.tv>
2020-02-14 13:40:49 -0700

    Forced choice

--- front-end/src/app/forced-choice/forced-choice.component.ts
+++ front-end/src/app/forced-choice/forced-choice.component.ts
@@ -12,8 +12,60 @@ export class ForcedChoiceComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentFace = this.facePaths[this.progress];
+    this.randomFaces = [];
+    for (let i = 0; i < this.numberOfOptions - 1; i++) {
+      let j = Math.floor(Math.random() * this.facePaths.length);
+      while (this.randomFaces.indexOf(this.facePaths[j]) > -1 || j == this.progress) {
+        j = Math.floor(Math.random() * this.facePaths.length);
+      }
+      this.randomFaces.push(this.facePaths[j]);
+    }
+    let j = Math.floor(Math.random() * this.numberOfOptions - 1);
+    this.randomFaces.splice(j, 0, this.currentFace);
+  }
 
+  numberOfOptions = 4; // Hard coded for now
+  progress : number = 0;
   score : number = 0;
 
+  isMemorizing : boolean = true;
+  showFeedback : boolean = false;
+  correctSelection : boolean;
+
+  currentFace : string;
+  randomFaces : any[] = [];
+  selectedFace : string;
+
+  selectFace(facePath : string) {
+    if (!this.showFeedback) {
+      if (facePath == this.currentFace) {
+        this.score++;
+        this.correctSelection = true;
+      } else {
+        this.correctSelection = false;
+      }
+      this.selectedFace = facePath;
+      this.showFeedback = true;
+    }
+  }
+
+  nextFace() {
+    this.showFeedback = false;
+    this.progress++;
+    this.isMemorizing = true;
+    this.selectedFace = null;
+    this.currentFace = this.facePaths[this.progress];
+    this.randomFaces = [];
+    for (let i = 0; i < this.numberOfOptions - 1; i++) {
+      let j = Math.floor(Math.random() * this.facePaths.length);
+      while (this.randomFaces.indexOf(this.facePaths[j]) > -1 || j == this.progress) {
+        j = Math.floor(Math.random() * this.facePaths.length);
+      }
+      this.randomFaces.push(this.facePaths[j]);
+    }
+    let j = Math.floor(Math.random() * this.numberOfOptions - 1);
+    this.randomFaces.splice(j, 0, this.currentFace);
+  }
 }
