5e091aa20faa78f5f1c4fec62cb391c08c7fc990
Alex Sandridge <asandridge@spotx.tv>
2020-02-18 14:05:01 -0700

    Same different

--- front-end/src/app/same-different/same-different.component.ts
+++ front-end/src/app/same-different/same-different.component.ts
@@ -12,8 +12,47 @@ export class SameDifferentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentFace = this.facePaths[this.progress];
+    this.randomFace = this.currentFace;
+    if (Math.random() > .5) {
+      this.randomFace = this.facePaths[Math.floor(Math.random() * this.facePaths.length)];
+    }
+  }
 
+  progress : number = 0;
   score : number = 0;
+  stage : number = 0; // memorize, choose, feedback
 
+  correctSelection : boolean;
+  currentFace : string;
+  randomFace : string;
+
+  selectFace(facePath : string) {
+    if (facePath == this.currentFace) {
+      this.score++;
+      this.correctSelection = true;
+    } else {
+      this.correctSelection = false;
+    }
+    this.stage = 2;
+  }
+
+  nextFace() {
+    this.stage = 0;
+    this.progress++;
+    this.currentFace = this.facePaths[this.progress];
+    this.randomFace = this.currentFace;
+    if (Math.random() > .5) {
+      this.randomFace = this.facePaths[Math.floor(Math.random() * this.facePaths.length)];
+    }
+  }
+
+  clickCard() {
+    if (this.stage == 0) {
+      this.stage = 1;
+    } else if (this.stage == 2) {
+      this.nextFace()
+    }
+  }
 }
