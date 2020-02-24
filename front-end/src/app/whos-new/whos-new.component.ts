59626da8d16b2f59488058046a39b475defb5f33
Connor Dowd <codo7827@colorado.edu>
2020-02-24 16:47:18 -0700

    Finally got display fixed, by and large got image display and randomization working too, as well as 5 page quiz with score display

--- front-end/src/app/whos-new/whos-new.component.ts
+++ front-end/src/app/whos-new/whos-new.component.ts
@@ -5,27 +5,37 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
   templateUrl: './whos-new.component.html',
   styleUrls: ['./whos-new.component.scss'],
 })
+
 export class WhosNewComponent implements OnInit {
   @Input() setNames : string;
   @Input() facePaths : string;
   @Output() finished = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-	  // start by shuffling three faces from basic set
-	  this.currentFace = this.facePaths[this.progress];
+	  // start by shuffling three faces from basic set 
+	  for (let i = 0; i < this.facePaths.length; i++) {
+        this.shuffledFaces.push(this.facePaths[i]);
+      }
 	  
 	  // then draw one from new set
-	  ;
-	  
-	  // lump all faces into new set, then shuffle them
-	  ;
+	  this.shuffledFaces.push(this.facePaths);
   }
 
   score : number = 0;
-  currentFace : string;
-  progress : number = 0;
-  correct_count: number = 0;
+  progress : number = 0; 
+  
+  shuffledFaces : any[] = [];
 
+  nextFaceSet() {
+	  this.progress++;
+	  
+	  // dump old images, and repeat initalization process
+	  for (let i = 0; i < 4; i++) {
+        this.shuffledFaces.splice(i, 1);
+      }
+	  
+	  this.ngOnInit(); 
+  }
 }
