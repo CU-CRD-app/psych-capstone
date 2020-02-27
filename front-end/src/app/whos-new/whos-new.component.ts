ec254aafd0d2d62285f631002dd43449009922e0
Connor Dowd <codo7827@colorado.edu>
2020-02-26 18:37:55 -0700

    Made visualization more acceptable, improved randomization mechanism, added scoring mechanism

--- front-end/src/app/whos-new/whos-new.component.ts
+++ front-end/src/app/whos-new/whos-new.component.ts
@@ -5,37 +5,54 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
   templateUrl: './whos-new.component.html',
   styleUrls: ['./whos-new.component.scss'],
 })
-
 export class WhosNewComponent implements OnInit {
   @Input() setNames : string;
   @Input() facePaths : string;
   @Output() finished = new EventEmitter<number>();
 
   constructor() {}
-
+ // style="height:220px; width:160px; margin: 0 auto; display: inline-block"
   ngOnInit() {
-	  // start by shuffling three faces from basic set 
+	  // Initialize set
 	  for (let i = 0; i < this.facePaths.length; i++) {
         this.shuffledFaces.push(this.facePaths[i]);
       }
 	  
-	  // then draw one from new set
-	  this.shuffledFaces.push(this.facePaths);
+      // then shuffle set by swapping randomly chosen points
+      for (let i = this.shuffledFaces.length - 1; i > 0; i -= 1) {
+          let j = Math.floor(Math.random() * (i + 1));
+          let temp = this.shuffledFaces[i];
+          this.shuffledFaces[i] = this.shuffledFaces[j];
+          this.shuffledFaces[j] = temp;
+      }
+      
+      // Replace one face with new, unseen one
+      // Must be one of the first four that get displayed
+      this.chosenNum = Math.floor(Math.random() * 4);
+      // this.shuffledFaces[chosenNum] = 
   }
 
   score : number = 0;
   progress : number = 0; 
+  chosenNum : number = 0;
   
   shuffledFaces : any[] = [];
+  leftovers : any[] = [];
 
-  nextFaceSet() {
-	  this.progress++;
-	  
+  nextFaceSet(input : number) {
+      this.progress++;
+      // determine if correct or not
+      if (input == this.chosenNum) {
+        this.score++;
+	  }
+      // else nothing
+      
 	  // dump old images, and repeat initalization process
-	  for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < this.facePaths.length; i++) {
         this.shuffledFaces.splice(i, 1);
       }
-	  
-	  this.ngOnInit(); 
+      
+      this.ngOnInit();
+      
   }
 }
