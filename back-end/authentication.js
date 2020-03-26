ffedb4298edb30dbb26ec852639beef30c86ac05
Han233666 <36578940+Han233666@users.noreply.github.com>
2020-03-26 12:57:48 -0600

    authentication in middleware

---
+++ back-end/authentication.js
@@ -0,0 +1,80 @@
+// JavaScript source code
+import jwt from 'jsonwebtoken';
+
+import dotenv from 'dotenv';
+
+import {
+
+  errorMessage, status,
+
+} from '../helpers/status';
+
+
+
+import env from '../../env';
+
+
+
+dotenv.config();
+
+
+
+/**
+
+   * Verify Token
+
+   * @param {object} req 
+
+   * @param {object} res 
+
+   * @param {object} next
+
+   * @returns {object|void} response object 
+
+   */
+
+
+
+const verifyToken = async (req, res, next) => {
+
+  const { token } = req.headers;
+
+  if (!token) {
+
+    errorMessage.error = 'Token not provided';
+
+    return res.status(status.bad).send(errorMessage);
+
+  }
+
+  try {
+
+    const decoded =  jwt.verify(token, process.env.SECRET);
+
+    req.user = {
+
+      email: decoded.email,
+
+      user_id: decoded.user_id,
+
+      first_name: decoded.first_name,
+
+      last_name: decoded.last_name,
+
+    };
+
+    next();
+
+  } catch (error) {
+
+    errorMessage.error = 'Authentication Failed';
+
+    return res.status(status.unauthorized).send(errorMessage);
+
+  }
+
+};
+
+
+
+export default verifyToken;
