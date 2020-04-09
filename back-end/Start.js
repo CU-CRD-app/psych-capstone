fe6d4b8cd981c31a890a222975e36e8cf19a7c83
Han233666 <36578940+Han233666@users.noreply.github.com>
2020-04-09 13:26:31 -0600

    babel (aka ES6)preset

---
+++ back-end/Start.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
+require("@babel/register")({
+  presets: ["@babel/preset-env"]
+});
+
+// Import the rest of our application.
+module.exports = require('C:/Users/Yin Han/Desktop/computer science/CSCI4308/Project Code/nodejsapi/app/routes/index.js')
