63cde39421683300a93b210d39c27088f9677b64
MiracleWKY <44417008+MiracleWKY@users.noreply.github.com>
2020-04-28 14:02:51 -0600

    Update send_email_test2.js

--- send_email_test2.js
+++ send_email_test2.js
@@ -18,12 +18,20 @@ async function main() {
   });
 
   // send mail with defined transport object
+  var target_email = "kewu6458@colorado.edu"
+  var confirm_code = Math.random().toString(36).substring(2, 8);
+  console.log(confirm_code)
   let info = await transporter.sendMail({
     from: '"Psyc team 👻" <foo@example.com>', // sender address
-    to: "kewu6458@colorado.edu", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>" // html body
+    to: target_email, // list of receivers
+    subject: "Email Verification", // Subject line
+    text: "" + confirm_code, // plain text body
+    html: '<html><head><style> * {box-sizing: border-box;}'
+    + 'body {font-family: Arial, Helvetica, sans-serif;}'
+    + 'header {background-color: #666;padding: 5px;text-align: center;font-size: 30px;color: white;}</style></head>'
+    + '<body> <h3><b> Your comfirmation code is: </b></h3>'
+    + '<header><h2>' + confirm_code + '</h2></header></body></html>'
+    + "None-Reply Email: You're receiving this email because " + target_email + " is trying to register on CU-Psychology mobile application."// html body
   });
 
   console.log("Message sent: %s", info.messageId);
