2bd223515327f049af15db1883b282683e72fec5
Alex Sandridge <asandridge@spotx.tv>
2020-04-29 15:36:59 -0600

    console log

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.post("/userData/", cors(corsOptions), function(req, res, next) {
 
 app.put("/checktoken/", cors(corsOptions), function(req, res, next){
     //TODO: actually implement token logic
-    if(typeof(req.body.token) !== undefined){
+    if(typeof(req.body.token) !== 'undefined'){
         res.status(200).json({message: "Valid token"});
     }
     else{
--- userData.js
+++ userData.js
@@ -39,6 +39,8 @@ module.exports = {
 
         let level = resDays.rows.length + preCount.rows.length + postCount.rows.length;
 
+        console.log(resDays.rows[resDays.rows.length - 1])
+        console.log(resDays.rows[resDays.rows.length - 1][nameface])
         /*if (resDays.rows[resDays.rows.length - 1][nameface] > -1 || resDays.rows[resDays.rows.length - 1][whosnew] > -1 || resDays.rows[resDays.rows.length - 1][memory] > -1 || resDays.rows[resDays.rows.length - 1][shuffle] > -1 || resDays.rows[resDays.rows.length - 1][forcedchoice] > -1 || resDays.rows[resDays.rows.length - 1][samedifferent] > -1) {
             level--;
         }*/
