bd5ff43a61acf48f9a0f6bff527de640f031efcf
Alex Sandridge <asandridge@spotx.tv>
2020-04-29 06:19:58 -0600

    take info out o login

--- login.js
+++ login.js
@@ -34,37 +34,10 @@ module.exports = {
             })
         }
 
-        //TODO: Only supports one race, changes will need to be support other races
-        let resDays = await pgClient.query("SELECT * FROM day WHERE userid = $1", [res.rows[0].userid]);
-    
-        let preCount = await pgClient.query("SELECT * FROM preassessment WHERE userid = $1", [res.rows[0].userid]);
-
-        let postCount = await pgClient.query("SELECT * FROM postassessment WHERE userid = $1", [res.rows[0].userid]);
-
-        if (resDays.rows[resDays.rows.length - 1][nameface] > -1 || resDays.rows[resDays.rows.length - 1][whosnew] > -1 || resDays.rows[resDays.rows.length - 1][memory] > -1 || resDays.rows[resDays.rows.length - 1][shuffle] > -1 || resDays.rows[resDays.rows.length - 1][forcedchoice] > -1 || resDays.rows[resDays.rows.length - 1][samedifferent] > -1) {
-            level--;
-        }
-
-        // make these a dict or array to also pass the date
-        let preScore = 0;
-        let postScore = 0;
-
-        if(preCount.rows.length > 0){
-            preScore = preCount.rows[0].score;
-        }
-
-        if(postCount.rows.length > 0){
-            postScore = postCount.rows[0].score;
-        }
-
         pgClient.end();
         //TODO: implement token
         let sendObject = {
-            days: resDays.rows,
-            token: res.rows[0].userid,
-            level: level,
-            pre: preScore,
-            post: postScore
+            token: res.rows[0].userid
         }
 
         return new Promise(function(resolve, reject){
