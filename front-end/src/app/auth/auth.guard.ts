bf5ea4e84b592f5b6874b9ef1007fc098c9c3483
Alex Sandridge <asandridge@spotx.tv>
2020-05-08 08:56:09 -0600

    auth guard fix

--- front-end/src/app/auth/auth.guard.ts
+++ front-end/src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 
 
@@ -42,7 +42,8 @@ export class AuthGuard implements CanActivate {
       })
     };
     return this.http.put("https://crossfacerecognition.herokuapp.com/checktoken/", {}, httpOptions)
-      .pipe(map((res) => res["message"] == 'Valid token' ? true : false));
+      .pipe(map((res) => true))
+      .pipe(catchError((err) => of(false)));
   }
 
 }
