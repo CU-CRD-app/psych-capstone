e86394cf938928fc4e2691e710364f35b500aee8
LiGhtime <35538966+LiGhtime@users.noreply.github.com>
2021-05-01 00:02:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ initialize.start()
 
 var app = express();
 
+# ------- REF: https://stackoverflow.com/questions/66057755/cors-issue-with-heroku-after-deploying
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+# -------
+
 app.use(bodyParser.json());
 
 //Ref: https://github.com/troygoode/node-cors-server/blob/master/server.js
