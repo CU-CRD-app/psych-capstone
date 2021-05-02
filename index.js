fbed1d9300c3a2aece905df55f01297c46803027
LiGhtime <35538966+LiGhtime@users.noreply.github.com>
2021-05-02 00:47:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -217,8 +217,8 @@ app.put("/getTrainingFaces/", cors(corsOptions), function(req, res, next){
                     images.push(new Buffer(data, 'binary').toString('base64'));
                     //random choose 7 pictures from Level-X
                     var total_num = fs.readdirSync(`./faces/white/training/level-${req.body.level - 1}`).length;
-                    var img_indices = [0, 1, 2, 3, 4, 5, 6, 7];
-                    // var img_indices = Array.from(Array(total_num).keys());
+                    // var img_indices = [0, 1, 2, 3, 4, 5, 6, 7];
+                    var img_indices = Array.from(Array(total_num).keys());
                     // var shuffled_indices = _.shuffle(img_indices);
                     for (var i = 0; i < 7; i++) {
                         random_index = img_indices[i];
