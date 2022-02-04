59a32f0a2fe5eae7ce1ec1598d030bd6af671b37
Ryan Drew <learnitall0@gmail.com>
2022-02-04 13:56:20 -0700

    Add testing environment for setting correct backend url

---
+++ front-end/src/environments/environment.testing.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    // IMPORTANT: MUST END IN FORWARD-SLASH
+    backendBaseUrl: "https://crossfacerecognition-testing.herokuapp.com/"
+  };
+  
